6ee561922d2f336e26e47b051757ce3ff7cb9cee
thewhobox <45664417+thewhobox@users.noreply.github.com>
2025-01-19 18:42:14 +0100

    add: check for tools

--- scripts/Build-KnxProd.ps1
+++ scripts/Build-KnxProd.ps1
@@ -1,13 +1,26 @@
-# if(-Not (Test-Path -Path platformio.ini)) {
-#     Set-Location ..
-# }
-# if(-Not (Test-Path -Path platformio.ini)) {
-#     Write-Host "Skript aus falschem Pfad ausgeführt..."
-#     timeout /T 20
-#     exit 1
-# }
-# $projectDir = Get-Location
+$toolsExist = Test-Path -PathType Leaf ~/bin/Kaenx.Creator.Console.exe
 
-$files = Get-ChildItem -Path data/*.ae-manu
-$files
-~/bin/Kaenx.Creator.Console publish data/$($files[0].Name)
+if (!$toolsExist) {
+    Write-Host "
+        Fuer das Setup fehlen die notwendigen OpenKNX-Tools oder sie sind veraltet..
+        Bitte das neuste Paket herunterladen (mindestens Version $checkVersion)
+
+            https://github.com/OpenKNX/Kaenx-Creator-Console/releases
+        
+        entpacken und das Readme befolgen.
+
+        Danach bitte dieses Script erneut starten.
+
+        ACHTUNG: Heutige Browser warnen vor dem Inhalt des OpenKNX-Tools Pakets, 
+                 weil es ausfuehrbare Programme und ein PowerShell-Skript enthaellt.
+                 Falls jemand dem Paketinhalt nicht traut, kann er sich OpenKNXproducer
+                 und bossac selber kompilieren und entsprechend installieren.
+    "
+    timeout /T -1
+}
+
+if ($toolsExist) {
+    $files = Get-ChildItem -Path data/*.ae-manu
+    ~/bin/Kaenx.Creator.Console publish data/$($files[0].Name)
+    timeout /T 20 
+}
